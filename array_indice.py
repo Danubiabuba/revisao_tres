36f8dc7a7462c7797a16671db09076e87381d273
Danubiabuba <danubiaalves184@gmail.com>
2024-04-09 09:08:09 -0300

    Resolução questão 1

---
+++ array_indice.py
@@ -0,0 +1,8 @@
+def imprimir_nomes():
+    nomes = ["Danubia","Rafael","Heitor","jose"]
+    print("1 -",nomes[0])
+    print("2 -",nomes[1])
+    print("3 -",nomes[2])
+    print("4 -",nomes[3])
+    
+imprimir_nomes()
